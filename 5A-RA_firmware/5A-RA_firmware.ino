ffa95bf0c6b4519f9aedc73a74b6f0a4405f7c8a
valentin betbeze <valentin.betbeze@gmail.com>
2023-06-07 21:23:36 +0300

    Updated inverse kinematic algorithm with effector orientation. Not yet tested.

--- 5A-RA_firmware/5A-RA_firmware.ino
+++ 5A-RA_firmware/5A-RA_firmware.ino
@@ -11,11 +11,22 @@
 #include <assert.h>
 #include <Servo.h>
 
+
+/*----------------- MACROS ----------------*/
 #define PI_DEG          180
 #define PI2_DEG         90
 #define PI4_DEG         45
 #define PI8_DEG         22.5
+#define DEG(x)          (180.0 * x / M_PI)
+#define RAD(x)          (M_PI * x / 180.0)
 
+// Motor data pins
+#define PIN_M1          7
+#define PIN_M2          10
+#define PIN_M2s         11
+#define PIN_M3          8
+#define PIN_M4          9
+#define PIN_M5          12
 /* Motor pulse width range
  Mx_min refers to the pulse width on the motor x, in microseconds, 
  corresponding to the minimum (0 degree) angle on the servo.
@@ -37,7 +48,7 @@
 #define M5_PW_MIN       390
 #define M5_PW_MAX       2370
 // Motor maximum angular range in degree
-#define RANGE_M1        PI_DEG    
+#define RANGE_M1        PI_DEG
 #define RANGE_M2        PI_DEG
 #define RANGE_M3        165       // 165 deg is the M3 angle at which the upper arm rests
                                   // on the lower arm in a planar contact configuration
@@ -48,15 +59,16 @@
 #define ANGLE_INIT_M2   PI_DEG
 #define ANGLE_INIT_M3   RANGE_M3
 #define ANGLE_INIT_M4   PI2_DEG
-#define ANGLE_INIT_M5   120       // Makes the effector positioned horizontally (ref: ground)
+#define ANGLE_INIT_M5   PI_DEG
 
 // Robot physical parameters
-#define ANGLE_INIT_LA   54        // Lower Arm initial angle, in degree
+#define ANGLE_INIT_LA   144       // Lower Arm initial angle, in degree
 #define ANGLE_INIT_UA   30        // Upper Arm initial angle, in degree
 #define INVR_TRANS_LA   1.65      // Lower Arm transmission inverse ratio
 #define INVR_TRANS_UA   2         // Upper Arm transmission inverse ratio
-#define HEIGHT_SHOULDER 151       // Shoulder's axis height (ref: ground), in mm
+#define HEIGHT_SHOULDER 136       // Shoulder's axis height (ref: ground), in mm
 #define LENGTH_LA       150       // Lower Arm length, in mm
+#define M3M4            25.56     // Interaxial distance between the axis of M3 and M4, in mm
 #define LENGTH_UA       186.76    // Upper Arm length, in mm
 #define LENGTH_WRIST    35        // Wrist length, in mm
 
@@ -66,33 +78,28 @@
 
 
 /*------------ DATA STRUCTURES ------------*/
-
 typedef struct {
   bool forward;
-  int t1;
-  int t2;
-  byte angle_t0;
-  byte angle_t1;
-  byte angle_t2;
-  byte angle_tf;
+  uint32_t t1;
+  uint32_t t2;
+  uint8_t angle_t0;
+  uint8_t angle_t1;
+  uint8_t angle_t2;
+  uint8_t angle_tf;
   int delta;
 }  motor_dynamics_t ;
 
 typedef struct  {
-  int x;
-  int y;
-  int z;
+  int16_t x;      // mm
+  int16_t y;      // mm
+  int16_t z;      // mm
+  int16_t yaw;    // deg
+  int16_t pitch;  // deg
+  // roll not available on current robot version
 } cartesian_position_t;
-
-typedef struct {
-  int radius;
-  int theta;
-  int phi;
-} polar_position_t;
 
 
 /*----------- GLOBAL VARIABLES ------------*/
-
 Servo M1;
 Servo M2;
 Servo M2s;
@@ -107,18 +114,15 @@ motor_dynamics_t dynamics_M4;
 motor_dynamics_t dynamics_M5;
 
 cartesian_position_t cartesian_pos;
-polar_position_t polar_pos;
 
-bool single_motor_mode = false;
-bool cartesian_mode = false;
-bool polar_mode = false;
-bool single_motor_running = false;
-bool five_axis_running = false;
 unsigned char motor_id = '\0';
+bool single_motor_mode    = false;
+bool cartesian_mode       = false;
+bool single_motor_running = false;
+bool five_axis_running    = false;
 
 
-/*---------- FUNCTION PROTOYPES -----------*/
-
+/*---------- FUNCTION PROTOTYPES ----------*/
 /* Initialise the servo motor angular position. Use only in setup().
  */
 static void initialise_robot(void);
@@ -129,27 +133,19 @@ static void initialise_robot(void);
  LA = Lower Arm
  UA = Upper Arm
  */
-static void get_angles_from_cartesian(const cartesian_position_t *pos);
-
-/* Provides the motor angles in degree required to reach the polar 
- position given as input. The function directly updates the angles for 
- each motor in their respective struct <motor_dynamics_t> (angle_tf). 
- LA = Lower Arm
- UA = Upper Arm
- */
-static void get_angles_from_polar(const polar_position_t *pos);
+static void get_angles_from_cartesian(cartesian_position_t *pos);
 
 /* Computation of the required motor_dynamics_t parameters for a 
  given distance with regards to desired effector speed and acceleration.
  @warning: The final angle `stopAngle` must be determined and assigned to
  the motor_dynamics_t variable prior to using the function.
  */
-static void compute_dynamics(motor_dynamics_t *dyn, const byte current_angle);
+static void compute_dynamics(motor_dynamics_t *dyn, const uint8_t current_angle);
 
 /* Computes the angle at a time t between t0 and TF so that the motor 
 accelerates and decelerates at a rate <ACC> while heading to stopAngle.
 */
-static byte compute_angle(const motor_dynamics_t *dyn, const long t);
+static uint8_t compute_angle(const motor_dynamics_t *dyn, const uint32_t t);
 
 /* Resets the robot to its idle configuration
  */
@@ -161,8 +157,7 @@ static void reset_robot(void);
 static bool is_delta_positive(void);
 
 
-/*-------------- PROGRAM MAIN -------------*/
-
+/*-------------- PROGRAM BODY -------------*/
 void setup() {
   Serial.begin(9600);
   initialise_robot();
@@ -176,15 +171,14 @@ void setup() {
 
 void loop() {
   uint32_t t0 = millis();
-  // Operate the robot
-  while (Serial.available() == 0) {
+  // Operate the robot until a user input is detected
+  while (!Serial.available()) {
     /* Runs only if delta is positive or equal to zero, practically
      meaning that the acceleration is high enough so that the motor(s) 
      can complete the entire distance within the time TF.
      */  
     if (is_delta_positive()) {
       uint32_t t = millis() - t0;
-
       if (single_motor_running) {
         switch (motor_id) {
           case '1': 
@@ -215,37 +209,25 @@ void loop() {
       }
     }
   }
-
+  /* An input has been detected, the robot is idle while the input
+   is being processed.
+   */
   single_motor_running = false;
   five_axis_running = false;
+  // Parse input
   String input = Serial.readString();
-  // Mode selection
   if (input == "motor\n") {
     single_motor_mode = true;
     cartesian_mode = false;
-    polar_mode = false;
     Serial.println("Single motor mode accessed. Format: MX.%%%");
-  }
-  else if (input == "cartesian\n") {
-    single_motor_mode = false;
-    cartesian_mode = true;
-    polar_mode = false;
-    Serial.println("Cartesian mode accessed. Format: SXXX.YYY.ZZZ");
-  }
-  else if (input == "polar\n") {
-    single_motor_mode = false;
-    cartesian_mode = false;
-    polar_mode = true;
-    Serial.println("Polar mode accessed. Format: RRR.STTT.PPP");
   }
   else if (input == "reset\n") {
     single_motor_mode = false;
     cartesian_mode = false;
-    polar_mode = false;
     reset_robot();
     Serial.println("The robot has been reset.");
   }
-  // If 'input' is not a mode entry, then it is a value entry
+  // If input is not a mode, then it is a value
   else {
     /* The number (ID as in Servo M<ID>) of the motor must be entered as 
       well as the desired position to reach, expressed in percentage of the  
@@ -290,7 +272,6 @@ void loop() {
     }
     // Format: SXXX.YYY.ZZZ
     else if (cartesian_mode) {
-      
       String position_buf = "";
       for (int i = 0; i < 4; i++) position_buf += input[i];
       cartesian_pos.x = position_buf.toInt();
@@ -314,32 +295,6 @@ void loop() {
       Serial.print("\tz=" + String(cartesian_pos.z) + '\n');
       five_axis_running = true;    
     }
-    //Format: RRR.STTT.PPP
-    else if (polar_mode) {
-      
-      String position_buf = "";
-      for (int i = 0; i < 3; i++) position_buf += input[i];
-      polar_pos.radius = position_buf.toInt();
-      position_buf = "";
-      for (int i = 4; i < 8; i++) position_buf += input[i];
-      polar_pos.theta = position_buf.toInt();
-      position_buf = "";
-      for (int i = 9; i < 12; i++) position_buf += input[i];
-      polar_pos.phi = position_buf.toInt();
-
-      get_angles_from_polar(&polar_pos);
-      compute_dynamics(&dynamics_M1, M1.read());
-      compute_dynamics(&dynamics_M2, M2.read());
-      compute_dynamics(&dynamics_M3, M3.read());
-      compute_dynamics(&dynamics_M4, M4.read());
-      compute_dynamics(&dynamics_M5, M5.read());
-
-      Serial.print("> Input position:");
-      Serial.print("\tr=" + String(polar_pos.radius));
-      Serial.print("\ttheta=" + String(polar_pos.theta));
-      Serial.print("\tphi=" + String(polar_pos.phi) + '\n');
-      five_axis_running = true;
-    } 
     else {
       Serial.println("Error(loop): Unknown command");
     }
@@ -354,13 +309,13 @@ void loop() {
 /*--------- FUNCTION DEFINITIONS ----------*/
 
 static void initialise_robot(void) {
-  M1.attach(7, M1_PW_MIN, M1_PW_MAX);
+  M1.attach(PIN_M1, M1_PW_MIN, M1_PW_MAX);
   /*
-  M2.attach(10, M2_PW_MIN, M2_PW_MAX);
-  M2s.attach(11, M2s_PW_MIN, M2s_PW_MAX);
-  M3.attach(8, M3_PW_MIN, M3_PW_MAX);
-  M4.attach(9, M4_PW_MIN, M4_PW_MAX);
-  M5.attach(12, M5_PW_MIN, M5_PW_MAX);
+  M2.attach(PIN_M2, M2_PW_MIN, M2_PW_MAX);
+  M2s.attach(PIN_M2s, M2s_PW_MIN, M2s_PW_MAX);
+  M3.attach(PIN_M3, M3_PW_MIN, M3_PW_MAX);
+  M4.attach(PIN_M4, M4_PW_MIN, M4_PW_MAX);
+  M5.attach(PIN_M5, M5_PW_MIN, M5_PW_MAX);
   */
 
   M1.write(ANGLE_INIT_M1);
@@ -371,33 +326,34 @@ static void initialise_robot(void) {
   M5.write(ANGLE_INIT_M5);
 }
 
-static void get_angles_from_cartesian(const cartesian_position_t *pos) {
+static void get_angles_from_cartesian(cartesian_position_t *pos) {
   if (pos == NULL) {
     Serial.print("Error(get_angles_from_cartesian): pos pointer is NULL");
     assert(pos);
   }
-  const float HYPOT_XY = sqrt(pow(pos->x, 2) + pow(pos->y, 2));
-  const float HYPOT = sqrt(pow(HYPOT_XY - LENGTH_WRIST, 2) + pow(pos->z - HEIGHT_SHOULDER, 2));
-
-  const float ALPHA1 = asin((pos->z - HEIGHT_SHOULDER) / HYPOT);
-  const float ALPHA2 = acos((pow(LENGTH_LA, 2) + pow(HYPOT, 2) - pow(LENGTH_UA, 2)) / (2 * HYPOT * LENGTH_LA));
-  const float ALPHA = (PI * (PI_DEG - ANGLE_INIT_LA) / PI_DEG - ALPHA1 - ALPHA2) * INVR_TRANS_LA;
-
-  const float BETA1 = acos((pow(LENGTH_LA, 2) + pow(LENGTH_UA, 2) - pow(HYPOT, 2)) / (2 * LENGTH_LA * LENGTH_UA));
-  const float BETA = (BETA1 - (ANGLE_INIT_UA * PI / PI_DEG)) * INVR_TRANS_UA;
-
-  const float GAMMA = atan(float(pos->x) / float(pos->y)) + PI / 2;
-
-  const float THETA1 = acos((pow(HYPOT, 2) + pow(LENGTH_UA, 2) - pow(LENGTH_LA, 2)) / (2 * HYPOT * LENGTH_UA));
-  const float THETA2 = -ALPHA1 + PI / 2;
-  const float THETA = PI - THETA1 - THETA2 - PI * (float)18 / PI_DEG;
-
-  const byte ANGLE_M1 = byte(round(PI_DEG * GAMMA / PI));
-  const byte ANGLE_M2 = byte(RANGE_M2 - round(PI_DEG * ALPHA / PI));
-  const byte ANGLE_M3 = byte(RANGE_M3 - round(PI_DEG * BETA / PI));
-  const byte ANGLE_M4 = PI2_DEG; // @note: Motor is disabled. To be modified w/ effector orientation update
-  const byte ANGLE_M5 = byte(RANGE_M5 - round(PI_DEG * THETA / PI));
-
+  // Get M5 coordinates
+  const float EFFECTOR_PROJ_XY = LENGTH_WRIST * sin(RAD(pos->pitch));
+  pos->x -= EFFECTOR_PROJ_XY * cos(RAD(pos->yaw));
+  pos->y -= EFFECTOR_PROJ_XY * sin(RAD(pos->yaw));
+  pos->z -= LENGTH_WRIST * cos(RAD(pos->pitch));
+  // Compute parameters
+  const float M2M5_XY = sqrt(pow(pos->x, 2) + pow(pos->y, 2));
+  const float M2M5 =  sqrt(pow(M2M5_XY, 2) + pow(pos->z - HEIGHT_SHOULDER, 2));
+  const float ALPHA_P = acos((pow(LENGTH_LA, 2) + pow(M2M5, 2) - pow(LENGTH_UA, 2)) / (2 * M2M5 * LENGTH_LA));
+  const float ALPHA = (3 * M_PI / 2) - asin((pos->z - HEIGHT_SHOULDER) / M2M5) - ALPHA_P;
+  const float BETA = acos((pow(LENGTH_LA, 2) + pow(LENGTH_UA, 2) - pow(M2M5, 2)) / (2 * LENGTH_LA * LENGTH_UA));
+  const float PHI = atan(float(pos->x) / float(pos->y));
+  const float EPSILON = ALPHA - acos(M3M4 / LENGTH_UA) - BETA;
+  const float V_X5 = cos(RAD(pos->yaw))*cos(PHI) - sin(RAD(pos->yaw))*sin(PHI);
+  const float V_Y5 = cos(EPSILON)*(cos(RAD(pos->yaw))*sin(PHI) + sin(RAD(pos->yaw))*cos(PHI)) - cos(RAD(pos->pitch))*sin(EPSILON);
+  const float V_Z5 = sin(EPSILON)*(cos(RAD(pos->yaw))*sin(PHI) + sin(RAD(pos->yaw))*cos(PHI)) + cos(RAD(pos->pitch))*cos(EPSILON);
+  // Compute joint angles
+  const byte ANGLE_M1 = round(RANGE_M1 / 2 - DEG(PHI));
+  const byte ANGLE_M2 = round(RANGE_M2 - INVR_TRANS_LA * (DEG(ALPHA) - ANGLE_INIT_LA));
+  const byte ANGLE_M3 = round(PI_DEG - INVR_TRANS_UA * (DEG(BETA) - ANGLE_INIT_UA));
+  const byte ANGLE_M4 = round(RANGE_M4 - DEG(atan(V_X5 / V_Z5)));
+  const byte ANGLE_M5 = round(RANGE_M5 - DEG(atan(V_Y5 / sqrt(pow(V_X5, 2) + pow(V_Z5, 2)))) );
+  // If within the work enveloppe, assign the angles
   if (ANGLE_M1 >= 0 && ANGLE_M1 <= RANGE_M1
       && ANGLE_M2 >= 0 && ANGLE_M2 <= RANGE_M2
       && ANGLE_M3 >= 0 && ANGLE_M3 <= RANGE_M3
@@ -414,45 +370,7 @@ static void get_angles_from_cartesian(const cartesian_position_t *pos) {
   }
 }
 
-static void get_angles_from_polar(const polar_position_t *pos) {
-  if (pos == NULL) {
-    Serial.print("Error(get_angles_from_polar): pos pointer is NULL");
-    assert(pos);
-  }
-  const float ALPHA1 = PI / 2 - pos->phi;
-  const float ALPHA2 = acos((pow(LENGTH_LA, 2) + pow(pos->radius, 2) - pow(LENGTH_UA, 2)) / (2 * pos->radius * LENGTH_LA));
-  const float ALPHA = ((pos->theta - ANGLE_INIT_LA) * PI / PI_DEG - ALPHA2 + PI / 2) * INVR_TRANS_LA;
-
-  const float BETA1 = acos((pow(LENGTH_LA, 2) + pow(LENGTH_UA, 2) - pow(pos->radius, 2)) / (2 * LENGTH_LA * LENGTH_UA));
-  const float BETA = (BETA1 - (ANGLE_INIT_UA * PI / PI_DEG)) * INVR_TRANS_UA;
-
-  const float THETA1 = acos((pow(pos->radius, 2) + pow(LENGTH_UA, 2) - pow(LENGTH_LA, 2)) / (2 * pos->radius * LENGTH_UA));
-  const float THETA2 = -1 * ALPHA1 + PI / 2;
-  const float THETA = PI - THETA1 - THETA2 - PI * (float)18 / PI_DEG;
-
-  byte ANGLE_M1 = pos->phi;
-  byte ANGLE_M2 = byte(RANGE_M2 - round(PI_DEG * ALPHA / PI));
-  byte ANGLE_M3 = byte(RANGE_M3 - round(PI_DEG * BETA / PI));
-  byte ANGLE_M4 = PI2_DEG; // @note same as get_angles_from_cartesian()
-  byte ANGLE_M5 = byte(RANGE_M5 - round(PI_DEG * THETA / PI));
-
-  if (ANGLE_M1 >= 0 && ANGLE_M1 <= RANGE_M1
-      && ANGLE_M2 >= 0 && ANGLE_M2 <= RANGE_M2
-      && ANGLE_M3 >= 0 && ANGLE_M3 <= RANGE_M3
-      && ANGLE_M4 >= 0 && ANGLE_M4 <= RANGE_M4
-      && ANGLE_M5 >= 0 && ANGLE_M5 <= RANGE_M5) {
-    dynamics_M1.angle_tf = ANGLE_M1;
-    dynamics_M2.angle_tf = ANGLE_M2;
-    dynamics_M3.angle_tf = ANGLE_M3;
-    dynamics_M4.angle_tf = ANGLE_M4;
-    dynamics_M5.angle_tf = ANGLE_M5;
-  } 
-  else {
-    Serial.println("Error(get_angles_from_polar): End position is outside of work enveloppe");
-  }
-}
-
-static void compute_dynamics(motor_dynamics_t *dyn, const byte current_angle) {
+static void compute_dynamics(motor_dynamics_t *dyn, const uint8_t current_angle) {
   if (dyn == NULL) {
     Serial.print("Error(compute_dynamics): dyn pointer is NULL");
     assert(dyn);
@@ -473,12 +391,12 @@ static void compute_dynamics(motor_dynamics_t *dyn, const byte current_angle) {
   dyn->angle_t2 = dyn->angle_t1 + SIGN * SPEED * (dyn->t2 - dyn->t1) / 1000.0;
 }
 
-static byte compute_angle(const motor_dynamics_t *dyn, const long t) {
+static uint8_t compute_angle(const motor_dynamics_t *dyn, const uint32_t t) {
   if (dyn == NULL) {
     Serial.print("Error(compute_angle): dyn pointer is NULL");
     assert(dyn);
   }
-  byte angle;
+  uint8_t angle;
   if (t <= dyn->t1) {
     angle = (dyn->forward == true) ? (pow(t / 1000.0, 2) * ACC / 2 + dyn->angle_t0) :
                                     (-pow(t / 1000.0, 2) * ACC / 2 + dyn->angle_t0);
